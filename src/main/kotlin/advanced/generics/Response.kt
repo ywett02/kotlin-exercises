05a48383b083d1e3bda4200107503135b99c6004
ywett02 <ivet.jurcikova@gmail.com>
2025-10-15 07:04:40 +0100

    Exercise: Generic Response

--- src/main/kotlin/advanced/generics/Response.kt
+++ src/main/kotlin/advanced/generics/Response.kt
@@ -1,20 +1,20 @@
 package advanced.generics.response
 
-sealed class Response<R, E>
-class Success<R, E>(val value: R) : Response<R, E>()
-class Failure<R, E>(val error: E) : Response<R, E>()
+sealed class Response<out R, out E>
+class Success<out R>(val value: R) : Response<R, Nothing>()
+class Failure<out E>(val error: E) : Response<Nothing, E>()
 
 fun usage() {
-    //val rs1 = Success(1)
-    //val rs2 = Success("ABC")
-    //val re1 = Failure(Error())
-    //val re2 = Failure("Error")
-    //
-    //val rs3: Success<Number> = rs1
-    //val rs4: Success<Any> = rs1
-    //val re3: Failure<Throwable> = re1
-    //val re4: Failure<Any> = re1
-    //
-    //val r1: Response<Int, Throwable> = rs1
-    //val r2: Response<Int, Throwable> = re1
+    val rs1 = Success(1)
+    val rs2 = Success("ABC")
+    val re1 = Failure(Error())
+    val re2 = Failure("Error")
+
+    val rs3: Success<Number> = rs1
+    val rs4: Success<Any> = rs1
+    val re3: Failure<Throwable> = re1
+    val re4: Failure<Any> = re1
+
+    val r1: Response<Int, Throwable> = rs1
+    val r2: Response<Int, Throwable> = re1
 }
