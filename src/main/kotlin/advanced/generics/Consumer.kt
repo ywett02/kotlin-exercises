2d9ef271db892095a2c9cc66ead1acd242917266
ywett02 <ivet.jurcikova@gmail.com>
2025-10-15 07:09:18 +0100

    Exercise: Generic Consumer

--- src/main/kotlin/advanced/generics/Consumer.kt
+++ src/main/kotlin/advanced/generics/Consumer.kt
@@ -1,31 +1,31 @@
 package advanced.generics.consumer
 
-abstract class Consumer<T> {
+abstract class Consumer<in T> {
     abstract fun consume(elem: T)
 }
 
-class Printer<T> : Consumer<T>() {
+class Printer<in T> : Consumer<T>() {
     override fun consume(elem: T) {
         // ...
     }
 }
 
-class Sender<T> : Consumer<T>() {
+class Sender<in T> : Consumer<T>() {
     override fun consume(elem: T) {
         // ...
     }
 }
 
 fun main(args: Array<String>) {
-    //val p1 = Printer<Number>()
-    //val p2: Printer<Int> = p1
-    //val p3: Printer<Double> = p1
-    //
-    //val s1 = Sender<Any>()
-    //val s2: Sender<Int> = s1
-    //val s3: Sender<String> = s1
-    //
-    //val c1: Consumer<Number> = p1
-    //val c2: Consumer<Int> = p1
-    //val c3: Consumer<Double> = p1
+    val p1 = Printer<Number>()
+    val p2: Printer<Int> = p1
+    val p3: Printer<Double> = p1
+
+    val s1 = Sender<Any>()
+    val s2: Sender<Int> = s1
+    val s3: Sender<String> = s1
+
+    val c1: Consumer<Number> = p1
+    val c2: Consumer<Int> = p1
+    val c3: Consumer<Double> = p1
 }
